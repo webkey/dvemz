24cc6ee474e4e34dab5953fc02f549340a9ccace
webkey <serhiy_i@ukr.net>
2017-04-27 20:33:37 +0300

    office_20170421_2030

---
+++ src/js/common.js
@@ -0,0 +1,205 @@
+/**
+ * !resize only width
+ * */
+var resizeByWidth = true;
+
+var prevWidth = -1;
+$(window).resize(function () {
+	var currentWidth = $('body').outerWidth();
+	resizeByWidth = prevWidth !== currentWidth;
+	if (resizeByWidth) {
+		$(window).trigger('resizeByWidth');
+		prevWidth = currentWidth;
+	}
+});
+/*resize only width end*/
+
+/**
+ * !device detected
+ * */
+var DESKTOP = device.desktop();
+//console.log('DESKTOP: ', DESKTOP);
+var MOBILE = device.mobile();
+//console.log('MOBILE: ', MOBILE);
+var TABLET = device.tablet();
+//console.log('MOBILE: ', MOBILE);
+/*device detected end*/
+
+/**
+ *  placeholder
+ * */
+function placeholderInit() {
+	$('[placeholder]').placeholder();
+}
+/*placeholder end*/
+
+/**
+ * !print
+ * */
+function printShow() {
+	$('.view-print').on('click', function (e) {
+		e.preventDefault();
+		window.print();
+	})
+}
+/*print end*/
+
+/**
+ * !sliders
+ * */
+function slidersInit() {
+	//images carousel
+	var $imagesCarousel = $('.images-slider');
+
+	if($imagesCarousel.length){
+		var slideCounterTpl = '' +
+			'<div class="slider-counter">' +
+				'<span class="slide-curr">0</span>/<span class="slide-total">0</span>' +
+			'</div>';
+
+		$imagesCarousel.each(function () {
+			var $currentImagesCarousel = $(this);
+			var $images = $currentImagesCarousel.find('.images-slider__list');
+			var $titles = $currentImagesCarousel.find('.flashes');
+			var dur = 200;
+
+			$images.on('init', function (event, slick) {
+				$(slick.$slider).append($(slideCounterTpl).clone());
+
+				$('.slide-total', $(slick.$slider)).text(slick.$slides.length);
+				$('.slide-curr', $(slick.$slider)).text(slick.currentSlide + 1);
+			});
+
+			$images.slick({
+				fade: false,
+				speed: dur,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				asNavFor: $titles,
+				// initialSlide: 2,
+				lazyLoad: 'ondemand',
+				infinite: true,
+				dots: true,
+				arrows: true
+			}).on('beforeChange', function (event, slick, currentSlide, nextSlider) {
+				$('.slide-curr', $(slick.$slider)).text(nextSlider + 1);
+			});
+
+			$titles.slick({
+				fade: true,
+				speed: dur,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				asNavFor: $images,
+				dots: false,
+				arrows: false
+			});
+
+		});
+	}
+}
+/*sliders end*/
+
+/**
+ * !footer at bottom
+ * */
+function footerBottom() {
+	var $footer = $('.footer');
+
+	if ($footer.length) {
+		$('.main').after($('<div class="spacer"></div>'));
+
+		setTimeout(function () {
+			layoutFooter();
+		}, 50);
+
+		$(window).on('resizeByWidth', function () {
+			layoutFooter();
+		});
+
+		function layoutFooter() {
+			// var footerHeight = $('.footer__holder', $footer).outerHeight();
+			var footerHeight = $($footer).outerHeight();
+			$footer.css({
+				'margin-top': -footerHeight
+			});
+
+			$('.spacer').css({
+				'height': footerHeight
+			});
+		}
+	}
+}
+/*footer at bottom end*/
+
+/**
+ * !form success for example
+ * */
+function formSuccessExample() {
+	var $form = $('.user-form form');
+
+	if ( $form.length ) {
+
+		$form.submit(function (event) {
+			var $thisForm = $(this);
+
+			if ($thisForm.parent().hasClass('success-form')) return;
+
+			event.preventDefault();
+
+			testValidateForm($thisForm);
+		});
+
+		// $(':text, input[type="email"], textarea', $form).on('keyup change', function () {
+		// 	var $form = $(this).closest('form');
+		// 	if ($form.parent().hasClass('error-form')) {
+		// 		testValidateForm($form);
+		// 	}
+		// })
+
+	}
+
+	function testValidateForm(form) {
+		var $thisFormWrap = form.parent();
+
+		var $inputs = $(':text, input[type="email"], input[type="password"], textarea', form);
+
+		var inputsLength = $inputs.length;
+		var inputsHasValueLength = $inputs.filter(function () {
+			return $(this).val().length;
+		}).length;
+
+		$thisFormWrap.toggleClass('error-form', inputsLength !== inputsHasValueLength);
+		$thisFormWrap.toggleClass('success-form', inputsLength === inputsHasValueLength);
+
+		$.each($inputs, function () {
+			var $thisInput = $(this);
+			var thisInputVal = $thisInput.val();
+			var $thisInputWrap = $thisInput.parent();
+
+			$thisInput.toggleClass('error', !thisInputVal.length);
+			$thisInput.toggleClass('success', !!thisInputVal.length);
+
+			$thisInputWrap.toggleClass('error', !thisInputVal.length);
+			$thisInputWrap.toggleClass('success', !!thisInputVal.length);
+		});
+	}
+}
+/* form success for example end */
+
+/** ready/load/resize document **/
+
+$(document).ready(function () {
+	placeholderInit();
+	printShow();
+	slidersInit();
+
+	footerBottom();
+	formSuccessExample();
+
+	$('#fullpage').fullpage({
+		verticalCentered: false
+		// navigation: true
+	});
+});
